49cf2d988d62261ea54a1e2d53e56ccf75e3a9ac
Nathan Diehl <33075406+NathanielDiehl@users.noreply.github.com>
2021-09-01 14:46:45 -0500

    Added Vector methods

--- Mathematics/Mathematics/Program.cs
+++ Mathematics/Mathematics/Program.cs
@@ -6,24 +6,18 @@ namespace Mathematics
     {
         static void Main(string[] args)
         {
-            /*Polynomial p1 = new Polynomial(1, 2, 3, 4, 5);
-            Console.WriteLine(p1.function(5));*/
-            //Polynomial p1 = new Polynomial(new List<Complex>{ new Complex(1, 5), new Complex(0, 5), new Complex(1)} );
-            
             Polynomial p = new Polynomial(5, 5, 5, 7);
             Polynomial d = Calculus.Derivative(p);
             Polynomial i = Calculus.Integral(d, 7);
 
-            Console.WriteLine(p);
+            /*Console.WriteLine(p);
             Console.WriteLine(d);
             Console.WriteLine(i);
-            Console.WriteLine(Calculus.Integral(d, 3, 1) );
+            Console.WriteLine(Calculus.Integral(d, 3, 1) );*/
 
-            //Console.WriteLine(p1.function(5) );
-
-            /*Complex c1 = new Complex(1, 5);
-            Complex c2 = new Complex(3, 4);
-            Console.WriteLine(c1/c2);*/
+            Vector v1 = new Vector(1, 2, 3, 4);
+            Vector v2 = new Vector(10, 20, 30);
+            //Console.WriteLine(v2 - v1);
         }
     }
 }
--- Mathematics/Mathematics/Vector.cs
+++ Mathematics/Mathematics/Vector.cs
@@ -8,24 +8,85 @@ namespace Mathematics
     {
         public List<double> vector;
         public int dimensions { get; private set; }
-        Vector(List<double> v)
+        public Vector(List<double> v)
         {
             vector = v;
             dimensions = v.Count;
         }
-        Vector(double[] v): this(new List<double>(v))
+        public Vector(double[] v) : this(new List<double>(v))
         {
+        }
+        public Vector(double a = Double.MaxValue, double b = Double.MaxValue, double c = Double.MaxValue, double d = Double.MaxValue, double e = Double.MaxValue, double f = Double.MaxValue, double g = Double.MaxValue, double h = Double.MaxValue, double i = Double.MaxValue, double j = Double.MaxValue, double k = Double.MaxValue)
+        {
+            vector = new List<double>();
+            double[] nums = new double[] { a, b, c, d, e, f, g, h, i, j, k };
+            int l = 0;
+            for (int spot = nums.Length - 1; spot >= 0; spot--)
+            {
+                if (nums[spot] != Double.MaxValue)
+                    break;
+                l++;
+            }
+            for (int spot = 0; spot < nums.Length - l; spot++)
+            {
+                vector.Add(nums[spot]);
+            }
+            dimensions = vector.Count;
         }
         public static Vector operator *(Vector v, double s)
         {
             List<double> newV = new List<double>();
 
-            for (int i = 0; i <= v.dimensions; i++)
+            for (int i = 0; i < v.dimensions; i++)
             {
                 newV.Add(v.vector[i] * s);
             }
 
             return new Vector(newV);
+        }
+        public static Vector operator *(double s, Vector v)
+        {
+            return v * s;
+        }
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector newV;
+            Vector shortV;
+            if (v1.dimensions > v2.dimensions)
+            {
+                shortV = v2;
+                newV = v1;
+            }
+            else
+            {
+                shortV = v1;
+                newV = v2;
+            }
+
+            for (int i = 0; i < shortV.dimensions; i++)
+            {
+                newV.vector[i] += shortV.vector[i];
+            }
+
+            return newV;
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return v1 + (-1 * v2);
+        }
+
+        public override string ToString()
+        {
+            String str = "<";
+
+            for (int i = 0; i < dimensions - 1; i++)
+            {
+                str += vector[i] + ", ";
+            }
+            str += vector[dimensions - 1] + ">";
+
+            return str;
         }
     }
 }
