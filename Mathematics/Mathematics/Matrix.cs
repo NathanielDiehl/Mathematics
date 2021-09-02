34b3b5d74e4e8bcb33efa0884feadaf6e6bb5374
NathanD15 <Nathan.P.Diehl@gmail.com>
2021-09-01 19:23:06 -0500

    Added matrix methods

--- Mathematics/Mathematics/Matrix.cs
+++ Mathematics/Mathematics/Matrix.cs
@@ -6,6 +6,56 @@ namespace Mathematics
 {
     internal class Matrix
     {
-        public List<List<int>> matrix;
+        public List<List<double>> matrix { get; private set; }
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        private static bool isSquare(List<List<double>> m)
+        {
+            if (m[0].Count != m.Count)
+                return false;
+            for (int i = 0; i < m.Count-1; i++)
+            {
+                if (m[i].Count != m[i + 1].Count)
+                    return false;
+            }
+                return true;
+        }
+        public Matrix(List<List<double>> m)
+        {
+            if (!isSquare(m))
+            {
+                throw new NonSquareMatrix();
+            }
+            matrix = m;
+            Row = matrix[0].Count;
+            Column = matrix.Count;
+        }
+        public Matrix(double[][] m)
+        {
+            List<List<double>> temp = new List<List<double>>();
+            for (int i = 0; i < m.Length; i++)
+            {
+                temp.Add(new List<double>(m[i]));
+            }
+            if (!isSquare(temp))
+            {
+                throw new NonSquareMatrix();
+            }
+            matrix = temp;
+            Row = matrix[0].Count;
+            Column = matrix.Count;
+        }
+    }
+
+    class NonSquareMatrix : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "matrix cannot be non square";
+            }
+        }
     }
 }
